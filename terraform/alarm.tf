b5f83cefbf23a70aef91050dcf6d6c90a9dc454f
dylanhs33 <dylanhs@btinternet.com>
2023-11-02 12:03:13 +0000

    added the alarms

--- terraform/alarm.tf
+++ terraform/alarm.tf
@@ -1,28 +1,53 @@
-# # Eventually needs cloudwatch alarm based on error handling in python function
-# #Example of creating an error metric and alarm
 
-# resource "aws_cloudwatch_log_metric_filter" "example_error" {
-#     name           = "Fred"
-#     pattern        = "MultipleOfThreeError"
-#     log_group_name = "/aws/lambda/ingestion/test"
-#     metric_transformation {
-#         name      = "loggingerror"
-#         namespace = "Errors"
-#         value     = "1"
-#   }
-# }
+#ClientError alarm filter
+resource "aws_cloudwatch_log_metric_filter" "ClientError" {
+    name           = "Ingestion function Client Error"
+    pattern        = "ClientError"
+    log_group_name = "/aws/lambda/ingestion-errors"
+    metric_transformation {
+        name      = "loggingClienterror"
+        namespace = "Errors"
+        value     = "1"
+  }
+}
+#ClientError alarm
+resource "aws_cloudwatch_metric_alarm" "ClientError_alarm" {
+  alarm_name                = "Ingestion function Client error alarm"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = 1
+  metric_name               ="loggingClienterror"
+  namespace                 = "Errors"
+  period                    = 30
+  statistic                 = "SampleCount"
+  threshold                 = 1
+  alarm_actions             = ["add someone's arn"]
+ }
 
-# resource "aws_cloudwatch_metric_alarm" "example_error_alarm" {
-#   alarm_name                = "terraform-Fred-error"
-#   comparison_operator       = "GreaterThanOrEqualToThreshold"
-#   evaluation_periods        = 1
-#   metric_name               ="loggingerror"
-#   namespace                 = "Errors"
-#   period                    = 30
-#   statistic                 = "SampleCount"
-#   threshold                 = 5
-#   alarm_actions             = ["arn recieved when setting up email"]
-# }
+ #ExceptionError alarm filter
+resource "aws_cloudwatch_log_metric_filter" "ExceptionError" {
+    name           = "Ingestion function Exception Error"
+    pattern        = "Exception"
+    log_group_name = "/aws/lambda/ingestion-errors"
+    metric_transformation {
+        name      = "loggingExceptionerror"
+        namespace = "Errors"
+        value     = "1"
+  }
+}
+#Exception alarm
+resource "aws_cloudwatch_metric_alarm" "ExceptionError_alarm" {
+  alarm_name                = "Ingestion function Exception error alarm"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = 1
+  metric_name               ="loggingExceptionerror"
+  namespace                 = "Errors"
+  period                    = 30
+  statistic                 = "SampleCount"
+  threshold                 = 1
+  alarm_actions             = ["add someone's arn"]
+ }
+
+
 # #example specific to exceution time error, doesn't require filter
 # resource "aws_cloudwatch_metric_alarm" "alert_code_execution_errors" {
 #   alarm_name                = "terraform-test-code-execution-error"
