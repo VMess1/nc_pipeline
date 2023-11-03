e556a477e7adc11cf0a56bffb5f7cfd9024cae46
dylanhs33 <dylanhs@btinternet.com>
2023-11-03 16:23:10 +0000

    fixing issues2

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -33,13 +33,4 @@ resource "aws_lambda_permission" "allow_eventbridge" {
 
 
 
-#TRANSFORMATION LAMBDA
-resource "aws_lambda_function" "lambda_transformation" {
-    function_name = var.lambda_transformation
-    role = aws_iam_role.lambda_ingestion_role.arn #CHANGE
-    s3_bucket = aws_s3_bucket.code_bucket_2.id
-    s3_key = aws_s3_object.lambda_code_2.key
-    handler = "testfunc.handler"    #update this with function name
-    runtime = "python3.11"
-    layers = [] #add layers
-}
+
