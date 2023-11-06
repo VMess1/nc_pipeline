d520043e4d19317efb4ca5a073eae57ac8f0737b
VMess1 <himynameisvick@hotmail.co.uk>
2023-11-06 11:01:05 +0000

    Removes old files, invokes lambda_handler function and formats tf files

--- terraform/data.tf
+++ terraform/data.tf
@@ -5,18 +5,9 @@ data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
 #INGESTION LAMBDA
-
-#zips the lambda function ready for use
-# data "archive_file" "lambda" {
-#   type        = "zip"
-#   source_file = "${path.module}/../src/tftest/testfunc.py"  #change this(function name)
-#   output_path = "${path.module}/../function.zip"  
-# }
-
-# Uncomment when ready to test the main function
 data "archive_file" "lambda" {
   type        = "zip"
-  source_file = "${path.module}/../src/extraction/extraction_lambda.py"  #change this(function name)
+  source_dir = "${path.module}/../src/extraction/" 
   output_path = "${path.module}/../function.zip"  
 }
 
