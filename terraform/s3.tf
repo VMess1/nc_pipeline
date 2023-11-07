ecb90f60ea77f4b4268f952c3035e49d5a28cd9a
dylanhs33 <dylanhs@btinternet.com>
2023-11-07 16:12:15 +0000

    cicd layers

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -15,7 +15,7 @@ resource "aws_s3_object" "lambda_code" {
 }
 
 resource "aws_s3_object" "layer_code" {
-  key    = "ingestion_function/layer_code.zip"      #updating(function name)
+  key    = "ingestion_function/layer_code.zip"      #updating(function name
   #source = "${path.module}/../layer_code.zip"   #update with proper 
   source = "./layer_code.zip"
   bucket = aws_s3_bucket.code_bucket.id
