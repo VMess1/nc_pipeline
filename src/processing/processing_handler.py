39ad3f0cebedda1161ce409241e9d7313a636d54
philupa <philippaclarkson@gmail.com>
2023-11-07 13:40:00 +0000

    writing to bucket updated

--- src/processing/processing_handler.py
+++ src/processing/processing_handler.py
@@ -21,15 +21,51 @@ def dim_remove_dates(data):
     return data
 
 
-def main(timestamp):
-    tables = ['design',
-              'staff',
-              'counterparty',
-              'currency',
-              'address',
-              'sales_order',
-              'departments'
-              ]
-    for table in tables:
-        table_data = get_csv_data(table, timestamp)
-        read_csv(table_data['Body'])
+def dim_join_department(staff_data, departments_data, timestamp):
+    result = pd.merge(staff_data, departments_data, on="department_id")
+
+    # new_df = staff_data.set_index('department_id').join(
+    # departments_data.set_index('department_id'))
+    return result
+
+
+def get_client():
+    s3 = boto3.client("s3", region_name="eu-west-2")
+    return s3
+
+
+def write_to_bucket(client, table_name, df, timestamp):
+    file_key = table_name + '/' + table_name + timestamp
+    parquet = df.to_parquet(
+        f's3://nc-group3-transformation-bucket/{df}/{df}{timestamp}.parquet',
+        engine='auto',
+        compression='snappy',
+        index=None,
+        partition_cols=None)
+    client.put_object(
+        Bucket="nc-group3-transformation-bucket", Key=file_key, Body=parquet)
+
+
+def main(event, context):
+    # tables = ['design',
+    #           'staff',
+    #           'counterparty',
+    #           'currency',
+    #           'address',
+    #           'sales_order',
+    #           'departments'
+    #           ]
+    for table_name in event['table_list']:
+        time_stamp = event['Timestamp']
+        table_data = get_csv_data(table_name, time_stamp)
+        df = read_csv(table_data['Body'])
+        if table_name == 'currency':
+            # dim_currency = dim_remove_dates(df, table_name)
+            s3 = get_client()
+            write_to_bucket(s3, table_name, df, time_stamp)
+        # elif table_name == 'design':
+        #     dim_design = dim_remove_dates(df)
+            # push_to_bucket()
+        # elif table_name == 'department':
+        #     staff_table_data = get_csv_data('staff', time_stamp)
+        #     dim_staff = dim_join_department(staff_table_data)
--- tests/test_processing/test_processing_handler.py
+++ tests/test_processing/test_processing_handler.py
@@ -1,4 +1,5 @@
-from src.processing.processing_handler import read_csv, dim_remove_dates
+from src.processing.processing_handler import (
+    read_csv, dim_remove_dates)  # ,  write_to_bucket)  # , dim_join_department
 from moto import mock_s3
 import boto3
 # import pytest
@@ -17,7 +18,7 @@ import pandas as pd
 
 class TestReadCSV:
     def test_csv_is_read(self):
-        output = read_csv('tests/test_processing/test.csv')
+        output = read_csv('tests/test_processing/test_payment.csv')
         assert output.iloc[1]['payment_id'] == 2
 
     @mock_s3
@@ -55,3 +56,65 @@ class TestDimRemoveDates:
             'currency_code': ['GBP', 'USD', 'EUR']
         })
         assert dim_remove_dates(test_dataframe).equals(expected_dataframe)
+
+
+# class TestWriteToBucket:
+#     @mock_s3
+#     def test_dataframe_is_saved_to_bucket(self):
+#         s3 = boto3.client("s3", region_name="eu-west-2")
+#         s3.create_bucket(
+#             Bucket='nc-group3-transformation-bucket',
+#             CreateBucketConfiguration={
+#                 "LocationConstraint": "eu-west-2"})
+#         test_df = pd.DataFrame(data={
+#             'currency_id': [1, 2, 3],
+#             'currency_code': ['GBP', 'USD', 'EUR'],
+#             'created_at': ['2022-11-03 14:20:49.962000',
+#                            '2022-11-03 14:20:49.962000',
+#                            '2022-11-03 14:20:49.962000'],
+#             'last_updated': ['2022-11-03 14:20:49.962000',
+#                              '2022-11-03 14:20:49.962000',
+#                              '2022-11-03 14:20:49.962000']
+#         })
+        # write_to_bucket(s3, 'currency',
+        #  test_df, '2022-11-03 14:20:49.962000')
+
+    # def test_join(self):
+    #     test_df_department = pd.DataFrame(data={
+    #         'department_id': [1, 2, 3],
+    #         'department_name': ['Sales', 'Purchasing', 'Production'],
+    #         'location': ['Manchester', 'Manchester', 'Leeds'],
+    #         'manager': ['Richard Roma', 'Naomi Lapaglia', 'Chester Ming'],
+    #         'created_at': ['2022-11-03 14:20:49.962000',
+    # '2022-11-03 14:20:49.962000', '2022-11-03 14:20:49.962000'],
+    #         'last_updated': ['2022-11-03 14:20:49.962000',
+    # '2022-11-03 14:20:49.962000', '2022-11-03 14:20:49.962000']
+    #     })
+    #     test_df_staff = pd.DataFrame(data={
+    #         'staff_id': [1, 2, 3],
+    #         'first_name': ['Jeremie', 'Deron', 'Jeanette'],
+    #         'last_name': ['Franey', 'Beier', 'Erdman'],
+    #         'department_id': [2, 3, 2],
+    #         'email_address': ['jeremie.franey@terrifictotes.com',
+    #  'deron.beier@terrifictotes.com', 'jeanette.erdman@terrifictotes.com'],
+    #         'created_at': ['2022-11-03 14:20:49.962000',
+    #  '2022-11-03 14:20:49.962000', '2022-11-03 14:20:49.962000'],
+    #         'last_updated': ['2022-11-03 14:20:49.962000',
+    #  '2022-11-03 14:20:49.962000', '2022-11-03 14:20:49.962000']
+    #     })
+    #     test_df_dim_staff = pd.DataFrame(data={
+    #         'staff_id': [1, 2, 3],
+    #         'first_name': ['Jeremie', 'Deron', 'Jeanette'],
+    #         'last_name': ['Franey', 'Beier', 'Erdman'],
+    #         'department_id': [2, 3, 2],
+    #         'email_address': ['jeremie.franey@terrifictotes.com',
+    #  'deron.beier@terrifictotes.com', 'jeanette.erdman@terrifictotes.com'],
+    #         'created_at': ['2022-11-03 14:20:49.962000',
+    #  '2022-11-03 14:20:49.962000', '2022-11-03 14:20:49.962000'],
+    #         'last_updated': ['2022-11-03 14:20:49.962000',
+    #  '2022-11-03 14:20:49.962000', '2022-11-03 14:20:49.962000'],
+
+    #     })
+
+    #     res = dim_join_department(test_df_staff, test_df_department)
+    #     pprint(res)
