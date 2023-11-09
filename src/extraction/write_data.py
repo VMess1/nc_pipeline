00aad5b5a41f0cf746c3ac3a61c0cba0186549b4
philupa <philippaclarkson@gmail.com>
2023-11-09 09:52:15 +0000

    made changes to csv conversion to match required format for pandas

--- src/extraction/write_data.py
+++ src/extraction/write_data.py
@@ -4,18 +4,17 @@ import boto3
 def convert_to_csv(table_name, data, headers):
     """converts returned sql data to csv string"""
     the_goods = ""
-    the_goods += table_name + "\n"
     for index, collumn in enumerate(headers):
         if index == len(headers) - 1:
             the_goods += f"{collumn[0]}\n"
         else:
-            the_goods += f"{collumn[0]}, "
+            the_goods += f"{collumn[0]},"
     for index, datum in enumerate(data):
         for index, dat in enumerate(datum):
             if index == len(datum) - 1:
                 the_goods += f"{dat}\n"
             else:
-                the_goods += f"{dat}, "
+                the_goods += f"{dat},"
     return the_goods
 
 
