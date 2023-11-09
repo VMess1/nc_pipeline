8165789e7c8a04e6b0745dc7047af7d733109dc2
philupa <philippaclarkson@gmail.com>
2023-11-09 16:44:21 +0000

    fact table is transformed

--- src/processing/fact_table_transformation.py
+++ src/processing/fact_table_transformation.py
@@ -1,9 +1,25 @@
 import pandas as pd
 
+
 def fact_sales_order(sale_order):
     new_dataframe = sale_order
-    new_dataframe['sales_record_id'] =new_dataframe.reset_index().index
-    new_dataframe.rename({'staff_id': 'sales_staff_id'}, axis='columns')
-    
-    print(new_dataframe)
-    return sale_order
+    new_dataframe['sales_record_id'] = new_dataframe.reset_index().index
+    new_dataframe = new_dataframe.rename(
+        {'staff_id': 'sales_staff_id'}, axis='columns')
+    new_dataframe['created_date'] = pd.DatetimeIndex(
+        sale_order['created_at']).date
+    new_dataframe['created_time'] = pd.DatetimeIndex(
+        sale_order['created_at']).time
+    new_dataframe['last_updated_date'] = pd.DatetimeIndex(
+        sale_order['last_updated']).date
+    new_dataframe['last_updated_time'] = pd.DatetimeIndex(
+        sale_order['last_updated']).time
+    new_dataframe = new_dataframe.drop(
+        columns="created_at", inplace=False, axis=1)
+    new_dataframe = new_dataframe.drop(
+        columns="last_updated", inplace=False, axis=1)
+    new_dataframe['agreed_delivery_date'] = pd.DatetimeIndex(
+        sale_order['agreed_delivery_date']).date
+    new_dataframe['agreed_payment_date'] = pd.DatetimeIndex(
+        sale_order['agreed_payment_date']).date
+    return new_dataframe
--- tests/test_processing/test_fact_table_transformation.py
+++ tests/test_processing/test_fact_table_transformation.py
@@ -1,8 +1,10 @@
 from src.processing.fact_table_transformation import fact_sales_order
-from tests.test_processing.dataframes import sales_order_dataframe, fact_sales_dataframe
+from tests.test_processing.dataframes import (sales_order_dataframe,
+                                              fact_sales_dataframe)
+
 
 def test_fact_sales_order_transformed_correctly():
     test_sale_order = sales_order_dataframe()
-    actual = fact_sales_order(test_sale_order)
-    expected = fact_sales_dataframe()
+    actual = fact_sales_order(test_sale_order).sort_index().sort_index(axis=1)
+    expected = fact_sales_dataframe().sort_index().sort_index(axis=1)
     assert actual.equals(expected)
