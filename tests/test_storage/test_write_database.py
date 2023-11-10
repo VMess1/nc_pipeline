f669a27e4eb1f123f8b185270ec3ed46fe15841f
averz87 <tjavery87@yahoo.com>
2023-11-10 11:16:43 +0000

    timestamp and credentials for storage added

---
+++ tests/test_storage/test_write_database.py
@@ -0,0 +1,55 @@
+from moto import mock_secretsmanager
+from dotenv import load_dotenv
+import pytest
+import boto3
+import os
+from datetime import datetime
+import json
+from pg8000.native import Connection
+from src.storage.write_database import (
+    get_credentials
+)
+
+load_dotenv()
+
+
+@pytest.fixture(scope="function")
+def aws_credentials():
+    os.environ["AWS_ACCESS_KEY_ID"] = "test"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "test"
+    os.environ["AWS_SECURITY_TOKEN"] = "test"
+    os.environ["AWS_SESSION_TOKEN"] = "test"
+    os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
+
+
+@pytest.fixture(scope="function")
+def test_connection():
+    return Connection(
+        user=os.environ["USER"],
+        host="localhost",
+        database=os.environ["TEST_DATABASE"],
+        password=os.environ["PASSWORD"],
+    )
+
+
+@pytest.fixture(scope="function")
+def secrets(aws_credentials):
+    with mock_secretsmanager():
+        yield boto3.client("secretsmanager", region_name="eu-west-2")
+
+
+class TestGetCredentials:
+    def test_get_credentials(self, secrets):
+        secret_id = "test_secret"
+        secret_values = {
+            "engine": "postgres",
+            "username": "test_user",
+            "password": "test_password",
+            "host": "test-database.us-west-2.rds.amazonaws.com",
+            "dbname": "test-database",
+            "port": "2222",
+        }
+        secrets.create_secret(Name=secret_id,
+                              SecretString=json.dumps(secret_values))
+        output = get_credentials(secret_id)
+        assert output == secret_values
