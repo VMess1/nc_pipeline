b164569c248555d1df1b5502e4ddbd85dd101c6b
averz87 <tjavery87@yahoo.com>
2023-11-14 11:38:35 +0000

    some dataframes added

--- src/storage/storage_handler.py
+++ src/storage/storage_handler.py
@@ -1,47 +1,45 @@
 import boto3
 from botocore.exceptions import ClientError
 import logging
-import re
 from datetime import datetime
 
-from src.storage.read_parquet import (get_file_list,
-                                      get_parquet_data,
-                                      compile_parquet_data)
+from src.storage.read_parquet import (compile_parquet_data)
 from src.storage.store_timestamp import (get_last_timestamp,
                                          write_current_timestamp)
 from src.storage.write_database import (get_credentials,
                                         get_con,
-                                        create_insert_statement)
+                                        run_insert_query)
 
 logger = logging.getLogger("LPY2Logger")
 logger.setLevel(logging.INFO)
 
 
-def get_client():
+def get_s3_client():
     s3 = boto3.client("s3", region_name="eu-west-2")
     return s3
 
-def main(event, context):
-    '''Cycles through all tables and updates warehouse when updates are found.'''
-    current_time = datetime.now().replace(microsecond=0)
-    credentials = get_credentials("OLAPCredentials")
-    con = get_con(credentials)
-    table_list = ['dim_date',
-                  'dim_currency',
-                  'dim_design',
-                  'dim_staff',
-                  'dim_location',
-                  'dim_counterparty',
-                  'fact_sales_order']
-    client = get_client()
-    timestamp = get_last_timestamp('last_insertion')
-    target_bucket = 'nc-group3-transformation-bucket'
-    for table_name in table_list:
-        dataframe = compile_parquet_data(client, target_bucket, table_name, timestamp)
-        insert_statement = create_insert_statement(table_name, dataframe)
 
+def main(event, context):
+    '''Cycles through tables and updates warehouse when updates are found.'''
     try:
-        pass
+        current_time = datetime.now().replace(microsecond=0)
+        credentials = get_credentials("OLAPCredentials")
+        con_warehouse = get_con(credentials)
+        table_list = ['dim_date',
+                    'dim_currency',
+                    'dim_design',
+                    'dim_staff',
+                    'dim_location',
+                    'dim_counterparty',
+                    'fact_sales_order']
+        s3client = get_s3_client()
+        timestamp = get_last_timestamp('last_insertion')
+        target_bucket = 'nc-group3-transformation-bucket'
+        for table_name in table_list:
+            dataframe = compile_parquet_data(
+                s3client, target_bucket, table_name, timestamp)
+            run_insert_query(con_warehouse, table_name, dataframe)
+        write_current_timestamp('last_insertion', current_time)
     except ClientError as err:
         if err.response["Error"]["Code"] == "NoSuchKey":
             logger.error("No such key")
--- tests/test_storage/test_storage_handler.py
+++ tests/test_storage/test_storage_handler.py
@@ -1,13 +1,22 @@
 from src.storage.storage_handler import (
     main)
-from moto import mock_s3
+from moto import mock_s3, mock_secretsmanager
 import boto3
 import pytest
 import pandas as pd
 import os
 from io import BytesIO
 import logging
-from botocore.exceptions import ClientError
+from tests.test_storage.data.seed_data import (
+    get_create_location_query,
+    get_create_sales_query,
+    get_seed_location_query,
+    get_seed_sales_query
+)
+from pg8000.native import Connection
+from unittest.mock import patch
+import datetime
+# from botocore.exceptions import ClientError
 
 
 logger = logging.getLogger('test')
@@ -23,10 +32,33 @@ def aws_credentials():
     os.environ["AWS_SESSION_TOKEN"] = "test"
     os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
 
+
+@pytest.fixture(scope="function")
+def test_connection():
+    return Connection(
+        user=os.environ["USER"],
+        host="localhost",
+        database=os.environ["TEST_DATA_WAREHOUSE"],
+        password=os.environ["PASSWORD"],
+    )
+
+
+@pytest.fixture(scope="function")
+def seeded_connection(test_connection):
+    test_connection.run("DROP TABLE IF EXISTS fact_test_sales_order;")
+    test_connection.run("DROP TABLE IF EXISTS dim_test_location;")
+    test_connection.run(get_create_location_query())
+    test_connection.run(get_create_sales_query())
+    test_connection.run(get_seed_location_query())
+    test_connection.run(get_seed_sales_query())
+    return test_connection
+
+
 @pytest.fixture(scope="function")
 def secrets(aws_credentials):
     with mock_secretsmanager():
         yield boto3.client("secretsmanager", region_name="eu-west-2")
+
 
 @pytest.fixture(scope='function')
 def mock_parquet_bucket(aws_credentials):
@@ -47,7 +79,12 @@ def mock_missing_parquet_bucket(aws_credentials):
 
 
 class TestBasicFunctionRuns:
-    def test_payment_table_updates(self, mock_parquet_bucket):
+    @patch('src.storage.storage_handler.get_credentials')
+    @patch('src.storage.storage_handler.get_con', return_value = seeded_connection)
+    @patch('src.storage.storage_handler.get_s3_client', return_value = mock_parquet_bucket)
+    @patch('src.storage.storage_handler.get_last_timestamp', return_value = datetime.datetime(2020,1,1,10,30,0))
+    @patch('src.storage.storage_handler.write_current_timestamp')
+    def test_dim_location_table_updates(self, mock_write, mock_time, mock_s3, mock_con, mock_creds, mock_parquet_bucket):
         test_df = pd.DataFrame(data={
             'location_id': [1, 2, 3],
             'address_line_1': ['street_1', 'street_2', 'street_3'],
@@ -59,27 +96,16 @@ class TestBasicFunctionRuns:
             'postal_code': ['A111AA', 'B222BB', 'C333CC'],
             'country': ['country_1', 'country_2', 'country_3'],
             'phone': ['1803 637401', '1803 637401', '1803 637401']
-            })
+        })
         test_string = test_df.to_string()
         mock_parquet_bucket.put_object(
             Bucket="nc-group3-transformation-bucket",
-            Key='payment/payment20221103150000.parquet',
+            Key='dim_location/dim_location2020110315000.parquet',
             Body=BytesIO(bytes(test_string, encoding='utf-8'))
         )
-        expected = (
-            'INSERT INTO dim_location \n'
-            '(location_id, address_line_1, address_line_2, district, '
-            'city, postal_code, country, phone) \n' 'VALUES \n'
-            '(1, street_1, place_1, district_1, city_1, A111AA, '
-            'country_1, 1803 637401)\n'
-            '(2, street_2, None, district_2, city_2, B222BB, '
-            'country_2, 1803 637401)\n'
-            '(3, street_3, None, district_3, city_3, C333CC, '
-            'country_3, 1803 637401)\n;'
-        )
-        actual = main(None, None)
-        assert actual == expected
-        
+        print(datetime.datetime(2020,1,1,10,30,0)) 
+    
+
 
 # class TestErrorHandling:
 
@@ -149,7 +175,7 @@ class TestBasicFunctionRuns:
     # #     monkeypatch.setattr(
     # #         'src.processing.processing_handler.get_client',
     # #         mock_get)
-    # #   test_event = {'table_list': ['currency'], 'timestamp': 20221103150000}
+    # #  test_event = {'table_list': ['currency'], 'timestamp': 20221103150000}
     # #     with caplog.at_level(logging.INFO):
     # #         main(test_event, None)
     # #         expected = ""
